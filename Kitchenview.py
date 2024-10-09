e0e5105d0b4146544237fefcce304e1f2ac61aa7
Gillezz <samgillette44@gmail.com>
2024-10-09 14:18:42 +0200

    fixed the "server not found error + adding a welcome message to the start screen"

--- Kitchenview.py
+++ Kitchenview.py
@@ -1,29 +1,35 @@
-# setting up flask framework 
+"""
+setting up flask framework 
+"""
 
 from flask import Flask, request
 
-kitchen = (__name__)
+kitchen = Flask(__name__)
+
+@kitchen.route('/')
+def home():
+    """
+    Starting site display
+    """
+    return "Welcome to the Burger Ordering Service!"
 
 @kitchen.route('/buy/<burger_name>')
-
 def receive_order(burger_name):
-    
-# add-ons list
+    """
+    order info about what items to get
+    """
+    # add-ons list
 
     add_ons = request.args.to_dict()
 
-# order details printed to the display image
+    # order details printed to the display image
 
     print(f'Ordering {burger_name} with the following add-ons: ')
 
-    for add_ons, content in add_ons.items():
-        print(f"- {add_ons}: {content}")
+    for add_on, content in add_ons.items():
+        print(f"- {add_on}: {content}")
 
-    return f"Order accepted {burger_name} with add-ons contents: {', '.join(add_ons.keys())}",
+    return f"Order accepted {burger_name} with add-ons contents: {', '.join(add_ons.keys())}"
 
 if __name__ == '__main__':
-    
     kitchen.run(debug = True, host = '0.0.0.0', port = 5000)
-
-
-
